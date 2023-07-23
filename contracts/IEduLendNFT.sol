2913b570aa20a13fd5075187cbe8f33b0f6830cc
sidarth16 <ssidarth1999@gmail.com>
2023-07-23 07:08:36 +0200

    interface

--- contracts/IEduLendNFT.sol
+++ contracts/IEduLendNFT.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.9;
 
 interface IEduLendNFT{
-    enum Status{ Ongoing, Completed, Defaulted};
 
-    function updateAdmin(address _admin) external{} 
+    function updateAdmin(address _admin) external;
 
-    function safeMint(address to, uint256 _courseId, uint256 _deadline) public {}
+    function safeMint(address to, uint256 _courseId, uint256 _deadline) external;
 
-    function updateStatus(uint256 _tokenId, Status status ) public{}
+    function setCompleted(uint256 _tokenId) external;
     
-    function getTokenIdStatus(uint256 _tokenId) public view returns(string memory status){}
+    function getTokenIdStatus(uint256 _tokenId) external view returns(string memory status);
        
 }
